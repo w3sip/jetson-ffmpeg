9fc370ad114c17d96aeec424980361d2812d4351
Alex Agranovsky <alex@agranovsky.com>
2023-03-23 15:19:46 -0400

    Allow local access for ffmpeg source
    Useful when no/limited connectivity

--- ffmpeg_dev/update_patch.sh
+++ ffmpeg_dev/update_patch.sh
@@ -4,9 +4,15 @@ SCRIPT_PATH="`dirname \"$0\"`"
 
 #clone
 rm -rf $SCRIPT_PATH/ffmpeg?.?
-git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.2 --depth=1 $SCRIPT_PATH/ffmpeg4.2
-git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.4 --depth=1 $SCRIPT_PATH/ffmpeg4.4
-git clone git://source.ffmpeg.org/ffmpeg.git -b release/6.0 --depth=1 $SCRIPT_PATH/ffmpeg6.0
+if [ -z $DEV_FFMPEG_SRC ]; then
+    git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.2 --depth=1 $SCRIPT_PATH/ffmpeg4.2
+    git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.4 --depth=1 $SCRIPT_PATH/ffmpeg4.4
+    git clone git://source.ffmpeg.org/ffmpeg.git -b release/6.0 --depth=1 $SCRIPT_PATH/ffmpeg6.0
+else
+    cp -r $DEV_FFMPEG_SRC/ffmpeg4.2 $SCRIPT_PATH/ffmpeg4.2
+    cp -r $DEV_FFMPEG_SRC/ffmpeg4.4 $SCRIPT_PATH/ffmpeg4.4
+    cp -r $DEV_FFMPEG_SRC/ffmpeg6.0 $SCRIPT_PATH/ffmpeg6.0
+fi
 
 #copy data
 cp -r $SCRIPT_PATH/4.2/* $SCRIPT_PATH/ffmpeg4.2/
