91a4bbcb93dd9f27168f7fcdb0f59b8b173716d7
Alex Agranovsky <alex@agranovsky.com>
2023-03-24 01:17:28 -0400

    Update patch creation with some logging

--- ffmpeg_dev/update_patch.sh
+++ ffmpeg_dev/update_patch.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 SCRIPT_PATH="`dirname \"$0\"`"
+SCRIPT_PATH=`realpath $SCRIPT_PATH`
 
+echo "Updating patches ..."
 
 #clone
 rm -rf $SCRIPT_PATH/ffmpeg?.?
 if [ -z $DEV_FFMPEG_SRC ]; then
+    echo "Cloning repositories to $SCRIPT_PATH"
     git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.2 --depth=1 $SCRIPT_PATH/ffmpeg4.2
     git clone git://source.ffmpeg.org/ffmpeg.git -b release/4.4 --depth=1 $SCRIPT_PATH/ffmpeg4.4
     git clone git://source.ffmpeg.org/ffmpeg.git -b release/6.0 --depth=1 $SCRIPT_PATH/ffmpeg6.0
 else
+    echo "Copying repositories from $DEV_FFMPEG_SRC to $SCRIPT_PATH"
     cp -r $DEV_FFMPEG_SRC/ffmpeg4.2 $SCRIPT_PATH/ffmpeg4.2
     cp -r $DEV_FFMPEG_SRC/ffmpeg4.4 $SCRIPT_PATH/ffmpeg4.4
     cp -r $DEV_FFMPEG_SRC/ffmpeg6.0 $SCRIPT_PATH/ffmpeg6.0
