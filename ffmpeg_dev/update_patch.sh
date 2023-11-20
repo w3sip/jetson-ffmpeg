40fb0a585eed0050de6ff9f3f98b256a8237dec9
w3sip <github@agranovsky.com>
2023-11-20 16:44:41 -0500

    Make patch creation safer

--- ffmpeg_dev/update_patch.sh
+++ ffmpeg_dev/update_patch.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
+set -x
+
 SCRIPT_PATH="`dirname \"$0\"`"
+SCRIPT_PATH=`realpath $SCRIPT_PATH`
+
+PATCH_DST=$1
+echo "Updating patches in ${PATCH_DST}"
+
+function checkRet() {
+    retVal=$1
+    msg=$2
+    if [ $retVal -ne 0 ]; then
+        echo "ERROR: ${msg}"
+        exit -1
+    fi
+}
+
+for longVer in "4.2.4" "4.4.2" "6.0"
+do
+    ver=`echo ${longVer} | head -c 3`
+    git clone git://source.ffmpeg.org/ffmpeg.git -b n${longVer} --depth=1 ${SCRIPT_PATH}/ffmpeg${ver}
+    checkRet $? "Cloning ${ver} to $SCRIPT_PATH/ffmpeg${ver}"
+    cp -r $SCRIPT_PATH/${ver}/* $SCRIPT_PATH/ffmpeg${ver}/
+    checkRet $? "copying version-specific files to $SCRIPT_PATH/ffmpeg${ver}/"
+    cp -r $SCRIPT_PATH/common/* $SCRIPT_PATH/ffmpeg${ver}/
+    checkRet $? "copying common files to $SCRIPT_PATH/ffmpeg${ver}/"
+    pushd $SCRIPT_PATH/ffmpeg${ver}
+    git add -A .
+    checkRet $? "git add"
+    git diff --cached > "${PATCH_DST}/ffmpeg${ver}_nvmpi.patch"
+    checkRet $? "creating patch to ${PATCH_DST}/ffmpeg${ver}_nvmpi.patch"
+    popd
+done
 
 
-#clone
-rm -rf $SCRIPT_PATH/ffmpeg?.?
-if [ -z $DEV_FFMPEG_SRC ]; then
-    git clone git://source.ffmpeg.org/ffmpeg.git -b n4.2.4 --depth=1 $SCRIPT_PATH/ffmpeg4.2
-    git clone git://source.ffmpeg.org/ffmpeg.git -b n4.4.2 --depth=1 $SCRIPT_PATH/ffmpeg4.4
-    git clone git://source.ffmpeg.org/ffmpeg.git -b n6.0   --depth=1 $SCRIPT_PATH/ffmpeg6.0
-else
-    cp -r $DEV_FFMPEG_SRC/ffmpeg4.2 $SCRIPT_PATH/ffmpeg4.2
-    cp -r $DEV_FFMPEG_SRC/ffmpeg4.4 $SCRIPT_PATH/ffmpeg4.4
-    cp -r $DEV_FFMPEG_SRC/ffmpeg6.0 $SCRIPT_PATH/ffmpeg6.0
-fi
-
-#copy data
-cp -r $SCRIPT_PATH/4.2/* $SCRIPT_PATH/ffmpeg4.2/
-cp -r $SCRIPT_PATH/4.4/* $SCRIPT_PATH/ffmpeg4.4/
-cp -r $SCRIPT_PATH/6.0/* $SCRIPT_PATH/ffmpeg6.0/
-cp -r $SCRIPT_PATH/common/* $SCRIPT_PATH/ffmpeg4.2/
-cp -r $SCRIPT_PATH/common/* $SCRIPT_PATH/ffmpeg4.4/
-cp -r $SCRIPT_PATH/common/* $SCRIPT_PATH/ffmpeg6.0/
-
-#
-pushd $SCRIPT_PATH/ffmpeg4.2
-git add -A .
-git diff --cached > $SCRIPT_PATH/../ffmpeg_patches/ffmpeg4.2_nvmpi.patch
-popd
-
-#
-pushd $SCRIPT_PATH/ffmpeg4.4
-git add -A .
-git diff --cached > $SCRIPT_PATH/../ffmpeg_patches/ffmpeg4.4_nvmpi.patch
-popd
-
-pushd $SCRIPT_PATH//ffmpeg6.0
-git add -A .
-git diff --cached > $SCRIPT_PATH/../ffmpeg_patches/ffmpeg6.0_nvmpi.patch
-popd
